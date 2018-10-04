121f58afa26e4f5c299f755927a1559446a64894
Mehedi <mehedi.peace@yahoo.com>
2018-10-04 03:34:05 +0000

    Task2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("external javaScript");
+console.log("External JavaScript");
+function sumArray(arr)
+{
+    var sum = 0;
+    arr.forEach(function(value,index)
+    {
+        sum = sum+value;
+    });
+    return sum;
+}
+console.log(sumArray([2,9,5,6,7]));
+console.log(sumArray([2,7,8,7]));
